4083dfef2030a9cad7ef33c367fb84701e126acf
Bastiaan en Esther (Vivo) <bastiaan.esther@gmail.com>
2019-05-19 13:02:14 +0200

    test of git pull werkt op pi

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # launcher.sh
 # navigeer naar project map, haal nieuwe code op en voer python script uit en ga dan terug naar home map.
-# Esther & Bastiaan 
+# Esther & Bastiaan van Dam
 
 cd /
 cd home/pi/Documents/projectmax
