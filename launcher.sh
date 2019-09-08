4b2424d465d443122dd92e6bb3170d9f858d3680
Bastiaan en Esther <bastiaan.esther@gmail.com>
2019-09-08 15:28:55 +0200

    Launcher script moet even wachten tot er internetverbinding is

--- launcher.sh
+++ launcher.sh
@@ -2,6 +2,11 @@
 # launcher.sh
 # navigeer naar project map, haal nieuwe code op en voer python script uit en ga dan terug naar home map.
 
+while ! ping -c 1 -W 1 github.com; do
+	echo "Wachten op github.com - Mogelijk nog niet verbonden met internet"
+	sleep 1
+done 
+
 cd /
 cd home/pi/Documents/projectmax
 sudo -u pi -i git --git-dir=/home/pi/Documents/projectmax/.git --work-tree=/home/pi/Documents/projectmax fetch origin
