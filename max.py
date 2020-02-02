295e916f0764ca517f28d2eb5e933c7279ce2e2b
Bastiaan en Esther (Vivo) <bastiaan.esther@gmail.com>
2020-02-02 13:29:45 +0100

    Led strip test

--- max.py
+++ max.py
@@ -5,13 +5,27 @@
 #|----------------------------------|
 
 # Import libraries:
-from max import laat_led
+from max import led_strip
 import pygame
+
+# Led
 from gpiozero import LED
+
+#Led Strip
+import RPi.GPIO as GPIO
+import Adafruit_WS2801
+import Adafruit_GPIO.SPI as SPI
+
+# Led Strip: Configure the count of pixels:
+PIXEL_COUNT = 30
+
+# Led Strip: Alternatively specify a hardware SPI connection on /dev/spidev0.0:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
 
 # variabelen:
 done = False    # Variabelen voor stoppen while loop
-led = LED(3)
  
 # Progamma:
 pygame.init()
@@ -21,4 +35,8 @@ while not done: # While loop stop als done true wordt (door Esc toets kyboard)
         if event.type == pygame.KEYDOWN:
             done = True
                
-    laat_led.knipperen(led)
+    # Clear all the pixels to turn them off.
+    pixels.clear()
+    pixels.show()  # Make sure to call show() after changing any pixels!
+    led_strip.rainbow_cycle(pixels, wait=0.02)
+    led_strip.brightness_decrease(pixels)
---
+++ max/led_strip.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  2 13:16:37 2020
+
+@author: BastiaanenEsther
+"""
+
+# https://raspberrytips.nl/ws2801-ledstrip/
+# Simple demo script for a WS2801/SPI-like addressable RGB LED lightstrip.
+
+import time
+import Adafruit_WS2801
+
+# Define the wheel function to interpolate between different hues.
+def wheel(pos):
+    if pos < 85:
+        return Adafruit_WS2801.RGB_to_color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Adafruit_WS2801.RGB_to_color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Adafruit_WS2801.RGB_to_color(0, pos * 3, 255 - pos * 3)
+
+def rainbow_cycle(pixels, wait=0.005):
+    for j in range(256): # one cycle of all 256 colors in the wheel
+        for i in range(pixels.count()):
+            pixels.set_pixel(i, wheel(((i * 256 // pixels.count()) + j) % 256) )
+        pixels.show()
+        if wait > 0:
+            time.sleep(wait)
+
+def brightness_decrease(pixels, wait=0.01, step=1):
+    for j in range(int(256 // step)):
+        for i in range(pixels.count()):
+            r, g, b = pixels.get_pixel_rgb(i)
+            r = int(max(0, r - step))
+            g = int(max(0, g - step))
+            b = int(max(0, b - step))
+            pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color( r, g, b ))
+        pixels.show()
+        if wait > 0:
+            time.sleep(wait)
